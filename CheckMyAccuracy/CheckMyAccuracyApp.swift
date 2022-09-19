98684cca6a93c1074297e8e6f1ab8c6c5b41c919
meetIgor <iigoor.s@gmail.com>
2022-09-19 21:36:44 +0300

    Initial Commit

---
+++ CheckMyAccuracy/CheckMyAccuracyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckMyAccuracyApp.swift
+//  CheckMyAccuracy
+//
+//  Created by igor s on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CheckMyAccuracyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
